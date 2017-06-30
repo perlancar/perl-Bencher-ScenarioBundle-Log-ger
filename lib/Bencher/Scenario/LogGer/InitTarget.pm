f9d81400e4031b4501952afe4d846c208fd21c7f
perlancar (on pc-home) <perlancar@gmail.com>
2017-07-01 05:51:33 +0700

    Add scenario: InitTarget

---
+++ lib/Bencher/Scenario/LogGer/InitTarget.pm
@@ -0,0 +1,40 @@
+package Bencher::Scenario::LogGer::InitTarget;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use File::Temp qw(tempfile);
+
+my ($fh, $fname) = tempfile();
+
+our $scenario = {
+    summary => 'Benchmark init_target()',
+    description => <<'_',
+
+Each participant performs 1000 times init_target() to a single package, with
+different configuration.
+
+_
+    participants => [
+        {name=>"default" ,
+         code_template => 'use Log::ger (); local %Log::ger::Global_Hooks = %Log::ger::Default_Hooks; for(1..1000) { Log::ger::init_target(target=>"package", target_args=>"main") }'},
+        {name=>"with LGO:Screen" ,
+         code_template => 'use Log::ger (); local %Log::ger::Global_Hooks = %Log::ger::Default_Hooks; use Log::ger::Output; Log::ger::Output->set("Screen"); for(1..1000) { Log::ger::init_target(target=>"package", target_args=>"main") }'},
+        {name=>"with LGO:File" ,
+         code_template => 'use Log::ger (); local %Log::ger::Global_Hooks = %Log::ger::Default_Hooks; use Log::ger::Output; Log::ger::Output->set("File", path=>'.qq('$fname').'); for(1..1000) { Log::ger::init_target(target=>"package", target_args=>"main") }'},
+        {name=>"with LGO:Composite (0 outputs)" ,
+         code_template => 'use Log::ger (); local %Log::ger::Global_Hooks = %Log::ger::Default_Hooks; use Log::ger::Output; Log::ger::Output->set("Composite"); for(1..1000) { Log::ger::init_target(target=>"package", target_args=>"main") }'},
+        {name=>"with LGO:Composite (2 outputs)" ,
+         code_template => 'use Log::ger (); local %Log::ger::Global_Hooks = %Log::ger::Default_Hooks; use Log::ger::Output; Log::ger::Output->set("Composite", outputs=>{Screen=>{}, File=>{conf=>{path=>'.qq('$fname').'}}}); for(1..1000) { Log::ger::init_target(target=>"package", target_args=>"main") }'},
+        {name=>"with LGO:Composite (2 outputs + pattern layouts)" ,
+         code_template => 'use Log::ger (); local %Log::ger::Global_Hooks = %Log::ger::Default_Hooks; use Log::ger::Output; Log::ger::Output->set("Composite", outputs=>{Screen=>{layout=>[Pattern=>{format=>"[%d] %m"}]}, File=>{conf=>{path=>'.qq('$fname').'}, layout=>[Pattern=>{format=>"[%d] [%P] %m"}]}}); for(1..1000) { Log::ger::init_target(target=>"package", target_args=>"main") }'},
+    ],
+    precision => 7,
+};
+
+1;
+# ABSTRACT:
