28a26d90c1267f74dad79e3b633591b220ba8d12
perlancar (on pc-home) <perlancar@gmail.com>
2017-07-01 05:31:16 +0700

    Add scenario: LayoutStartup

---
+++ lib/Bencher/Scenario/LogGer/LayoutStartup.pm
@@ -0,0 +1,36 @@
+package Bencher::Scenario::LogGer::LayoutStartup;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %layout_modules = (
+    Pattern => {format=>'[%d] %m'},
+    LTSV => {},
+    JSON => {},
+    YAML => {},
+);
+
+our $scenario = {
+    modules => {
+    },
+    participants => [
+        {name=>"baseline", perl_cmdline => ["-e1"]},
+
+        map {
+            (
+                +{
+                    name => "load-$_",
+                    module => "Log::ger::Layout::$_",
+                    perl_cmdline => ["-mLog::ger::Layout::$_", "-e1"],
+                },
+            )
+        } sort keys %layout_modules,
+    ],
+};
+
+1;
+# ABSTRACT:
